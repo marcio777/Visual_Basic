b63265722bc8b3ea70ef284bc66c9bebd4cbea86
marcio777 <marcio.infra@hotmail.com>
2018-01-29 21:25:43 -0200

    Aula4

---
+++ Aula4_Variavies_Numerica/Aula_04/Aula_04/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt_nascimento = New System.Windows.Forms.TextBox()
+        Me.txt_hoje = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cmdcalcular = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txt_nascimento
+        '
+        Me.txt_nascimento.Location = New System.Drawing.Point(6, 100)
+        Me.txt_nascimento.Name = "txt_nascimento"
+        Me.txt_nascimento.Size = New System.Drawing.Size(211, 20)
+        Me.txt_nascimento.TabIndex = 0
+        '
+        'txt_hoje
+        '
+        Me.txt_hoje.Location = New System.Drawing.Point(367, 100)
+        Me.txt_hoje.Name = "txt_hoje"
+        Me.txt_hoje.Size = New System.Drawing.Size(211, 20)
+        Me.txt_hoje.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cmdcalcular)
+        Me.GroupBox1.Controls.Add(Me.txt_nascimento)
+        Me.GroupBox1.Controls.Add(Me.txt_hoje)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(584, 556)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Idade"
+        '
+        'cmdcalcular
+        '
+        Me.cmdcalcular.Location = New System.Drawing.Point(204, 230)
+        Me.cmdcalcular.Name = "cmdcalcular"
+        Me.cmdcalcular.Size = New System.Drawing.Size(158, 38)
+        Me.cmdcalcular.TabIndex = 2
+        Me.cmdcalcular.Text = "calcular"
+        Me.cmdcalcular.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(608, 580)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents txt_nascimento As TextBox
+    Friend WithEvents txt_hoje As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents cmdcalcular As Button
+End Class
---
+++ Aula4_Variavies_Numerica/Aula_04/Aula_04/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Dim nome As String
+    Dim Nascimento As Integer
+    Dim AnoAtual As Integer
+    Dim Total As Integer
+    Dim idade As String
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txt_nascimento.TextChanged
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        nome = InputBox("Qual e o seu Nome?", "Nome")
+        txt_hoje.Text = Date.Today.Year
+    End Sub
+
+    Private Sub cmdcalcular_Click(sender As Object, e As EventArgs) Handles cmdcalcular.Click
+        Nascimento = txt_nascimento.Text
+        AnoAtual = txt_hoje.Text
+        Total = AnoAtual - Nascimento
+        idade = Total ' Converter para string para exibir o resultado
+        MessageBox.Show("Olá " + nome + ", Esse ano você completa " + idade + " anos", "Resultado", MessageBoxButtons.OK)
+    End Sub
+End Class
