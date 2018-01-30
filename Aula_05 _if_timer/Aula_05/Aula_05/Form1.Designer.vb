3e5d7150cb55b6753ee5bec8428b79ac595a36ce
marcio777 <marcio.infra@hotmail.com>
2018-01-29 22:04:21 -0200

    Aula05

---
+++ Aula_05 _if_timer/Aula_05/Aula_05/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.cmdcap = New System.Windows.Forms.Button()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'cmdcap
+        '
+        Me.cmdcap.Location = New System.Drawing.Point(12, 92)
+        Me.cmdcap.Name = "cmdcap"
+        Me.cmdcap.Size = New System.Drawing.Size(101, 35)
+        Me.cmdcap.TabIndex = 0
+        Me.cmdcap.Text = "Captura"
+        Me.cmdcap.UseVisualStyleBackColor = True
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(12, 47)
+        Me.MaskedTextBox1.Mask = "00:00"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(101, 20)
+        Me.MaskedTextBox1.TabIndex = 1
+        Me.MaskedTextBox1.ValidatingType = GetType(Date)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(238, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 36)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Hora"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(929, 155)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmdcap)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmdcap As Button
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Aula_05 _if_timer/Aula_05/Aula_05/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Dim Alarme As Date
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Label1.Text = TimeOfDay
+        If Alarme = Label1.Text Then
+            Timer1.Enabled = False
+            MessageBox.Show("Alarme da Hora")
+        End If
+    End Sub
+
+    Private Sub cmdcap_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdcap.Click
+        Alarme = MaskedTextBox1.Text
+        Timer1.Enabled = True
+    End Sub
+End Class
